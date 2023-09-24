52df04a7046ad1ef404da310599c6a1579389ece
kedmar <m.kedzierski76@gmail.com>
2023-09-24 17:35:48 +0200

    add Button Component from Ui. Schadcn

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,14 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-   return <p>Hello Admin Dashboard</p>;
+   return (
+      <div>
+         <p>Hello Admin Dashboard</p>
+         <div>
+            <Button size="default" variant="outline">
+               Click Me
+            </Button>
+         </div>
+      </div>
+   );
 }
