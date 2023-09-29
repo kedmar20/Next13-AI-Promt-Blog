995b7689555ee40dba6e5b0a9b14bd560d0394b1
Kedmar20 <m.kedzierski76@gmail.com>
2023-09-29 07:24:40 +0200

    finish modal component

---
+++ components/modals/store-modal.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { Modal } from "@components/ui/modal";
+
+export const StoreModal = () => {
+   const storeModal = useStoreModal();
+
+   <Modal
+      title="Create store"
+      description="Add a new store to manage products ans categories"
+      isOpen={storeModal.isOpen}
+      onClose={storeModal.onClose}
+   >
+      Future Create Store Form
+   </Modal>;
+};
